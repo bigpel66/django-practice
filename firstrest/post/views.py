3591f2c710071c1445e1623c28d21de1293fb244
bigpel66 <bigpel66@gmail.com>
2019-11-08 16:45:24 +0900

    ViewSet testing

--- firstrest/post/views.py
+++ firstrest/post/views.py
@@ -1,7 +1,18 @@
 from post.models import Post
 from post.serializer import PostSerializer
 from rest_framework import viewsets
+from rest_framework import renderers
+from rest_framework.decorators import action
+from django.http import HttpResponse
+
+# class PostViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        return HttpResponse("Yap!")
